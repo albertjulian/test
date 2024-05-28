05b191cf76158d206ac5b4acc690ad145cfdc075
albertjulian <albertjulian1993@gmail.com>
2024-05-28 10:32:20 +0700

    Final

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# test
+# Soal 1
+
+![Result](https://github.com/albertjulian/test/Soal_1.PNG?raw=true)
+
+# Soal 2
+
+![Result](https://github.com/albertjulian/test/Soal_2.PNG?raw=true)
+
+Kompleksitas kodingan ini adalah O(n), di mana n adalah panjang dari string expression.
+
+Penjelasan:
+
+Iterasi dilakukan sepanjang string expression, sehingga kompleksitasnya adalah O(n).
+Pada setiap iterasi, kita melakukan operasi push atau pop pada stack, yang memiliki kompleksitas O(1).
+Sehingga total kompleksitas kodingan ini adalah O(n).
+
+# Soal 3
--- soal_1.js
+++ soal_1.js
@@ -1,36 +1,36 @@
 function calculateWeights(arrString) {
-    const weights = {};
-    let i = 0;
-    while (i < arrString.length) {
-        let j = i;
-        while (j < arrString.length && arrString[j] === arrString[i]) {
-            j++;
-        	const substr = arrString.slice(i, j);
-            const weight = (arrString.charCodeAt(i) - 'a'.charCodeAt(0) + 1) * substr.length;
-        	weights[substr] = weight;
-        }
-        i++;
+  const weights = {};
+  let i = 0;
+  while (i < arrString.length) {
+    let j = i;
+    while (j < arrString.length && arrString[j] === arrString[i]) {
+      j++;
+      const substr = arrString.slice(i, j);
+      const weight = (arrString.charCodeAt(i) - 'a'.charCodeAt(0) + 1) * substr.length;
+      weights[substr] = weight;
     }
-    return weights;
+    i++;
+  }
+  return weights;
 }
 
 function weightedStrings(s, queries) {
-    const allWeighteds = calculateWeights(s);
-    const results = [];
-    for (let q of queries) {
-        let found = false;
-        for (let weight of Object.values(allWeighteds)) {
-            if (q === weight) {
-                results.push("Yes");
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            results.push("No");
-        }
+  const allWeighteds = calculateWeights(s);
+  const results = [];
+  for (let q of queries) {
+    let found = false;
+    for (let weight of Object.values(allWeighteds)) {
+      if (q === weight) {
+        results.push("Yes");
+        found = true;
+        break;
+      }
     }
-    return results;
+    if (!found) {
+      results.push("No");
+    }
+  }
+  return results;
 }
 
 const allString = "abbcccd";
